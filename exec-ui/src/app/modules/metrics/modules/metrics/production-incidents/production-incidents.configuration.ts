0b3b6c5ef7891a2e11732c258df34baeae7edefe
Subodh <subodh.battina@gmail.com>
2018-06-26 16:48:13 -0500

    Changes to labels and tooltips for Production Incidents Widget

--- exec-ui/src/app/modules/metrics/modules/metrics/production-incidents/production-incidents.configuration.ts
+++ exec-ui/src/app/modules/metrics/modules/metrics/production-incidents/production-incidents.configuration.ts
@@ -3,11 +3,11 @@ export class ProductionIncidentsConfiguration {
   public static previewHeading = 'Production Incidents';
   public static detailHeading = 'Production Incidents';
   public static graphHeading = 'Production Incidents';
-  public static buildingBlockLabel = 'Production Incidents';
+  public static buildingBlockLabel = 'Incidents';
   public static description = 'Sum of severity 1, 2, 3, 3C and 3D production incidents occurring over the last 90 ' +
     'days. An incident is any event that is not part of the standard operation of a service and that causes or may ' +
     'cause an interruption to, or a reduction in, the quality of that service.';
 
-  public static auxilliaryIdentifier = 'mean-time-to-resolve';
+  public static auxilliaryIdentifier = '(MTTR)';
   public static auxilliaryFigureHeading = 'Mean Time to Resolve';
 }
--- exec-ui/src/app/modules/metrics/modules/shared/components/metric-total-reporting/metric-total-reporting.component.ts
+++ exec-ui/src/app/modules/metrics/modules/shared/components/metric-total-reporting/metric-total-reporting.component.ts
@@ -8,6 +8,7 @@ import * as d3 from 'd3';
 })
 export class TotalReportingComponent implements OnChanges {
   @Input() public totalReporting: number = null;
+  @Input() public isComponentList: boolean;
 
   constructor() { }
 
