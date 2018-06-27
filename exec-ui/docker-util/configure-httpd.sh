9661874a624bca71d21d2e3c9fea2212998445e9
matthewbeasley <beardsley.ma@gmail.com>
2018-06-27 14:29:09 -0500

    add ssl support

--- exec-ui/docker-util/configure-httpd.sh
+++ exec-ui/docker-util/configure-httpd.sh
@@ -1,5 +1,7 @@
 sed -in 's/#LoadModule proxy_module/LoadModule proxy_module/' /usr/local/apache2/conf/httpd.conf
 sed -in 's/#LoadModule proxy_http_module/LoadModule proxy_http_module/' /usr/local/apache2/conf/httpd.conf
 sed -in 's/#LoadModule access_compat_module/LoadModule access_compat_module/' /usr/local/apache2/conf/httpd.conf
+sed -in 's/#LoadModule ssl_module/LoadModule ssl_module/g' /usr/local/apache2/conf/httpd.conf
+echo "SSLProxyEngine on" >> /usr/local/apache2/conf/httpd.conf
 echo "ProxyPass /api $API_URL/api" >> /usr/local/apache2/conf/httpd.conf
 echo "ProxyPassReverse /api $API_URL/api" >> /usr/local/apache2/conf/httpd.conf
