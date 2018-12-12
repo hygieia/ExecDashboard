1b7522d74acb1c69be5382b2d73bd6b302bed6fd
Subodh <subodh.battina@gmail.com>
2018-12-12 14:37:00 -0600

    Updated the trend slope indicator calculation logic

--- exec-core/src/main/java/com/capitalone/dashboard/exec/model/RollupMetricDetail.java
+++ exec-core/src/main/java/com/capitalone/dashboard/exec/model/RollupMetricDetail.java
@@ -5,7 +5,8 @@ import java.util.*;
 public class RollupMetricDetail extends MetricDetails {
     protected static final String MTTR = "mttr";
     protected static final String TYPE = "type";
-    protected static final int TREND_SLOPE_BUCKETS = 6;
+    protected static final int BUCKET_SIZE = 15;
+    protected static final int RANGE = 90;
 
     protected void updateSummary(MetricDetails metricDetails) {
         if (metricDetails.isProcessed()) {return;}
@@ -64,7 +65,8 @@ public class RollupMetricDetail extends MetricDetails {
     }
 
     protected Double calculateTrendSlope(List<MetricTimeSeriesElement> timeSeriesElements) {
-        double values[] = new double[TREND_SLOPE_BUCKETS];
+        int arraySize = RANGE/BUCKET_SIZE;
+        double values[] = new double[arraySize];
 
         // Get the total counts in each bucket
         for (MetricTimeSeriesElement t: timeSeriesElements) {
@@ -82,46 +84,42 @@ public class RollupMetricDetail extends MetricDetails {
 
             double metricCountValue = (metricCount != null)?metricCount.getValue():0.0;
 
-            if (daysAgo <= 15) {
+            if (daysAgo <= BUCKET_SIZE) {
                 values[values.length-1] += metricCountValue;
-            } else if (daysAgo <= 31) {
+            } else if (daysAgo <= BUCKET_SIZE*2) {
                 values[values.length-2] += metricCountValue;
-            } else if (daysAgo <= 46) {
+            } else if (daysAgo <= BUCKET_SIZE*3) {
                 values[values.length-3] += metricCountValue;
-            } else if (daysAgo <= 61) {
+            } else if (daysAgo <= BUCKET_SIZE*4) {
                 values[values.length-4] += metricCountValue;
-            } else if (daysAgo <= 76) {
+            } else if (daysAgo <= BUCKET_SIZE*5) {
                 values[values.length-5] += metricCountValue;
             } else {
                 values[values.length-6] += metricCountValue;
             }
-        };
+        }
 
         return deriveTrend(values);
     }
 
     protected Double deriveTrend(double values[]) {
-        double trend1 = values[1] - values[0]; // compare 1st bucket and 2nd bucket
+        double previousTrend = 0.0;
+        double cumulativeTrend = 0.0;
 
-        double trend2 = values[2] - values[1]; // compare 2nd bucket and 3rd bucket
+        for (int i=0; i<values.length; i++) {
+            double currentTrend = 0.0;
+            if (i > 0) {
+                currentTrend = (values[i] > 0)?(values[i] - values[i-1]):values[i];
+            }
 
-        double trend12 = trend2 + trend1; // compare the trends between 1st, 2nd, and 2nd, 3rd buckets.
+            cumulativeTrend = currentTrend + previousTrend;
+            previousTrend = currentTrend;
+        }
 
-        double trend3 = values[3] - values[2]; // compare 3rd and 4th buckets.
-
-        double trend123 = trend3 + trend12; // compare the net trend between 1,2,3 buckets, with the trend between buckets 3,4.
-
-        double trend4 = values[4] - values[3]; // compare 4th and 5th buckets.
-
-        double trend1234 = trend4 + trend123; // compare the net trend between 1,2,3,4 buckets, with the trend between buckets 4,5
-
-        double trend5 = values[5] - values[4]; // compare 5th and 6th buckets.
-
-        double trend12345 = trend5 + trend1234; // compare the net trend between 1,2,3,4,5 buckets, with the trend between buckets 5,6
-
-        if (trend12345 == 0.0) {
+        if (cumulativeTrend == 0.0) {
             return 0.0;
         }
-        return (trend12345 > 0.0)?1.0:-1.0;
+
+        return (cumulativeTrend > 0.0)?1.0:-1.0;
     }
 }
