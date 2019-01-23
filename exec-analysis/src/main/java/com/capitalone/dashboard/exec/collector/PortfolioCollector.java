fcca9d7f1c0eb60af1318df459716097955357c9
nameisaravind <Aravindhan.murugan@capitalone.com>
2019-01-23 15:25:42 -0500

    Unit test coverage collector (#33)
    * Unit Test Coverage widget - Collector and UI Changes
    
    * Uncommented the commented lines by mistake
    
    * unit test widgets
    
    * review comments
    
    * review comments
    
    * review comments

--- exec-analysis/src/main/java/com/capitalone/dashboard/exec/collector/PortfolioCollector.java
+++ exec-analysis/src/main/java/com/capitalone/dashboard/exec/collector/PortfolioCollector.java
@@ -65,13 +65,17 @@ public class PortfolioCollector implements Runnable {
 
     private final IncidentCollector incidentCollector;
 
+    private UnitTestCoverageCollector unitTestCoverageCollector;
+
     @Autowired
     public PortfolioCollector(TaskScheduler taskScheduler, PortfolioRepository portfolioRepository,
                                 PortfolioCollectorSetting setting,
                                 SCMCollector scmCollector,
                                 LibraryPolicyCollector libraryPolicyCollector,
                                 StaticCodeAnalysisCollector staticCodeAnalysisCollector,
-                                IncidentCollector incidentCollector) {
+                                IncidentCollector incidentCollector,
+                                UnitTestCoverageCollector unitTestCoverageCollector) {
+
         this.taskScheduler = taskScheduler;
         this.portfolioRepository = portfolioRepository;
         this.setting = setting;
@@ -79,6 +83,7 @@ public class PortfolioCollector implements Runnable {
         this.libraryPolicyCollector = libraryPolicyCollector;
         this.staticCodeAnalysisCollector = staticCodeAnalysisCollector;
         this.incidentCollector = incidentCollector;
+        this.unitTestCoverageCollector = unitTestCoverageCollector;
     }
 
     /**
@@ -102,7 +107,7 @@ public class PortfolioCollector implements Runnable {
         libraryPolicyCollector.collect(sparkSession, javaSparkContext, portfolioList);
         incidentCollector.collect(sparkSession, javaSparkContext, portfolioList);
         staticCodeAnalysisCollector.collect(sparkSession, javaSparkContext, portfolioList);
-
+        unitTestCoverageCollector.collect(sparkSession, javaSparkContext, portfolioList);
         sparkSession.close();
         javaSparkContext.close();
     }
